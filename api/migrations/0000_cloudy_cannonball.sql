10447612933c1a3afe155e53d4c44982a22188ba
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-02-16 23:50:16 -0500

    refactor: go back to spa

--- api/migrations/0000_cloudy_cannonball.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `messages` (
-	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	`name` text NOT NULL,
-	`message` text NOT NULL,
-	`createdAt` text NOT NULL,
-	`updatedAt` text NOT NULL
-);
